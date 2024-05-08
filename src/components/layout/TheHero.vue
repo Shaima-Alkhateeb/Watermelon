d7864da2f18af7c728cfa28968437e1c5ec70a13
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2024-05-09 00:17:01 +0300

    add button in home page

--- src/components/layout/TheHero.vue
+++ src/components/layout/TheHero.vue
@@ -2,16 +2,10 @@
   <!-- hero component -->
   <the-banner isHero titleHero>
     <template #image>
-      <img
-        class="image"
-        alt="Palestine"
-        src="../../assets/img/hero/HeroImage2.png"
-      />
+      <img class="image" alt="Palestine" src="../../assets/img/hero/HeroImage2.png" />
     </template>
     <template #title>
-      <p
-        class="poetsen-one-regular 2xl:text-8xl xl:text-8xl lg:text-8xl md:text-6xl xs:text-5xl"
-      >
+      <p class="poetsen-one-regular 2xl:text-8xl xl:text-8xl lg:text-8xl md:text-6xl xs:text-5xl">
         Palestine
       </p>
 
@@ -31,21 +25,13 @@
     <!-- History Section -->
     <section class="pt-16">
       <!-- <div class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 "> -->
-      <div
-        class="grid gap-x-8 gap-y-2 grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1"
-      >
-        <div
-          class="top-0 right-0 z-10 order-sm-last xs:mt-3 grid xs:justify-items-center"
-        >
-          <img
-            alt="user header"
-            src="../../assets/img/homepage/Map-of-ancient-Palestine3.png"
-            class="rounded-2xl shadow-2xl xs:w-96 sm:ml-4 xs:ml-4"
-          />
+      <div class="grid gap-x-8 gap-y-2 grid-cols-3 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1">
+        <div class="top-0 right-0 z-10 order-sm-last xs:mt-3 grid xs:justify-items-center">
+          <img alt="user header" src="../../assets/img/homepage/Map-of-ancient-Palestine3.png"
+            class="rounded-2xl shadow-2xl xs:w-96 sm:ml-4 xs:ml-4" />
         </div>
         <p
-          class="text-white 2xl:text-8xl xl:text-8xl lg:text-8xl md:text-6xl sm:text-4xl xs:text-3xl font-bold poetsen-one-regular col-span-2 md:col-span-1 p-2.5 w-4/5 md:w-10/12 sm:w-11/12 xs:w-11/12 m-auto"
-        >
+          class="text-white 2xl:text-8xl xl:text-8xl lg:text-8xl md:text-6xl sm:text-4xl xs:text-3xl font-bold poetsen-one-regular col-span-2 md:col-span-1 p-2.5 w-4/5 md:w-10/12 sm:w-11/12 xs:w-11/12 m-auto">
           Walk through history
         </p>
         <!-- <p
@@ -55,9 +41,7 @@
         through his
       </p> -->
       </div>
-      <div
-        class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 sm:my-3 xs:my-3"
-      >
+      <div class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 sm:my-3 xs:my-3">
         <div class="p-2.5 w-4/5 md:w-10/12 sm:w-11/12 xs:w-11/12 m-auto">
           <p class="text-lg sm:text-lg xs:text-base text-white">
             Palestine's history, steeped in antiquity, boasts a rich tapestry of
@@ -69,24 +53,14 @@
         </div>
 
         <div class="p-2.5 m-auto z-10">
-          <img
-            alt="user header"
-            src="../../assets/img/homepage/Palestinian4.jpg"
-            width=""
-            class="rounded-2xl shadow-2xl"
-          />
+          <img alt="user header" src="../../assets/img/homepage/Palestinian4.jpg" width=""
+            class="rounded-2xl shadow-2xl" />
         </div>
       </div>
-      <div
-        class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1"
-      >
+      <div class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1">
         <div class="p-2.5 m-auto z-10">
-          <img
-            alt="user header"
-            src="../../assets/img/homepage/Palestinian3-2.jpeg"
-            width="565"
-            class="rounded-2xl shadow-2xl"
-          />
+          <img alt="user header" src="../../assets/img/homepage/Palestinian3-2.jpeg" width="565"
+            class="rounded-2xl shadow-2xl" />
         </div>
         <div class="p-2.5 w-4/5 md:w-10/12 sm:w-11/12 xs:w-11/12 m-auto">
           <p class="text-lg sm:text-lg xs:text-base text-white">
@@ -109,6 +83,11 @@
       </div>
     </section>
 
+    <div class="flex flex-row justify-center my-10 drop-shadow-xl">
+      <Button icon="pi pi-angle-double-right" text raised rounded aria-label="Filter" class="button-icon-home-page 2xl:p-8 md:p-8 sm:p-4 xs:p-4"  />
+      <Button label="READ ABOUT HISTORY" rounded class="button-home-page" @click="ReadAboutHistory" />
+    </div>
+
     <!-- Food Section -->
     <section class="2xl:mt-72 sm:mt-36 xs:mt-36">
       <!-- <p
@@ -118,18 +97,13 @@
         estine food
       </p> -->
 
-      <div
-        class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 sm:my-3 xs:my-3"
-      >
+      <div class="grid gap-x-8 gap-y-2 grid-cols-2 md:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 sm:my-3 xs:my-3">
         <div class="p-2.5 w-4/5 md:w-10/12 sm:w-11/12 xs:w-11/12 m-auto">
           <p
-            class="text-white 2xl:text-8xl xl:text-8xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-3xl font-bold z-10 absolute poetsen-one-regular"
-          >
+            class="text-white 2xl:text-8xl xl:text-8xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-3xl font-bold z-10 absolute poetsen-one-regular">
             Palestine food market
           </p>
-          <p
-            class="text-lg sm:text-lg xs:text-base text-white 2xl:mt-40 xl:mt-40 lg:mt-28 sm:mt-16 xs:mt-16"
-          >
+          <p class="text-lg sm:text-lg xs:text-base text-white 2xl:mt-40 xl:mt-40 lg:mt-28 sm:mt-16 xs:mt-16">
             The Palestinian food market is a vibrant tapestry of flavors,
             aromas, and traditions that reflect the region's rich culinary
             heritage. From bustling souks to family-owned stalls, the market
@@ -137,9 +111,7 @@
             tantalize the senses and showcase the diversity of Palestinian
             cuisine.
           </p>
-          <p
-            class="text-lg sm:text-lg xs:text-base text-white 2xl:mt-10 sm:mt-5 xs:mt-5"
-          >
+          <p class="text-lg sm:text-lg xs:text-base text-white 2xl:mt-10 sm:mt-5 xs:mt-5">
             Visitors to the Palestinian food market are treated to a feast for
             the senses, with vendors skillfully preparing traditional dishes
             like falafel, hummus, and maqluba, alongside an assortment of sweets
@@ -151,16 +123,16 @@
         </div>
 
         <div class="p-2.5 m-auto z-9">
-          <img
-            alt="user header"
-            src="../../assets/img/homepage/fruit1.jpg"
-            width=""
-            class="rounded-2xl shadow-2xl"
-            style=""
-          />
+          <img alt="user header" src="../../assets/img/homepage/fruit1.jpg" width="" class="rounded-2xl shadow-2xl"
+            style="" />
         </div>
       </div>
     </section>
+
+    <div class="flex flex-row justify-center my-10 drop-shadow-xl">
+      <Button icon="pi pi-angle-double-right" text raised rounded aria-label="Filter" class="button-icon-home-page 2xl:p-8 md:p-8 sm:p-4 xs:p-4"  />
+      <Button label="READ ABOUT FOOD" rounded class="button-home-page" @click="ReadAboutFood" />
+    </div>
 
     <!-- Eid Section -->
     <section class="2xl:mt-72 sm:mt-36 xs:mt-36">
@@ -170,69 +142,49 @@
         Eid in Pales
       </p> -->
       <div class="lg:p-0 md:p-2.5 sm:p-2.5 w-full md:w-full sm:w-11/12 xs:w-11/12 m-auto">
+        <div
+          class="grid grid-cols-6 gap-4 2xl:grid-cols-6 xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-2 lg:mx-0">
+          <p
+            class="text-white 2xl:text-8xl xl:text-8xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-3xl font-bold float-right 2xl:mr-10 xl:mr-10 lg:mr-10 poetsen-one-regular 2xl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-start-1 md:col-span-3 sm:col-start-1 sm:col-span-3 xs:col-span-3 lg:text-center lg:mb-4 xs:mb-4">
+            Eid in Palestine
+          </p>
 
-
-      <div
-        class="grid grid-cols-6 gap-4 2xl:grid-cols-6 xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-2 sm:grid-cols-2 xs:grid-cols-2 lg:mx-0 "
-      >
-        <p
-          class="text-white 2xl:text-8xl xl:text-8xl lg:text-6xl md:text-6xl sm:text-4xl xs:text-3xl font-bold float-right 2xl:mr-10 xl:mr-10 lg:mr-10  poetsen-one-regular 2xl:col-span-6 xl:col-span-6 lg:col-span-6 md:col-start-1 md:col-span-3 sm:col-start-1 sm:col-span-3 xs:col-span-3 lg:text-center lg:mb-4 xs:mb-4 "
-        >
-          Eid in Palestine
-        </p>
-
-        <!-- src="../../assets/img/homepage/maamoul4.jpeg" -->
-        <!-- src="../../assets/img/homepage/maamoul.png" -->
-        <img
-        :src="imageSrc"
-          alt="maamoul"
-          :class="{
+          <!-- src="../../assets/img/homepage/maamoul4.jpeg" -->
+          <!-- src="../../assets/img/homepage/maamoul.png" -->
+          <img :src="imageSrc" alt="maamoul" :class="{
             'md:rounded-2xl': isMd || isSm || isXs,
             'rounded-r-2xl': !isMd && is2xl,
           }"
-          class="rounded-r-2xl 2xl:rounded-l-none xl:rounded-l-none lg:rounded-l-none md:rounded-l-2xl sm:rounded-l-2xl xs:rounded-l-2xl shadow-2xl 2xl:mt-60 xl:mt-60 lg:mt-60 "
-        />
-        <div class="2xl:col-span-2 xl:col-span-2 lg:col-span-2 2xl:ml-10 xl:ml-10 lg:ml-10 md:m-auto">
-          <img
-          alt="Jerusalem in Eid prayer"
-          src="../../assets/img/homepage/Jerusalem-in-Eid-prayer-final.png"
-          class="rounded-2xl shadow-2xl  "
-          />
+            class="rounded-r-2xl 2xl:rounded-l-none xl:rounded-l-none lg:rounded-l-none md:rounded-l-2xl sm:rounded-l-2xl xs:rounded-l-2xl shadow-2xl 2xl:mt-60 xl:mt-60 lg:mt-60" />
+          <div class="2xl:col-span-2 xl:col-span-2 lg:col-span-2 2xl:ml-10 xl:ml-10 lg:ml-10 md:m-auto">
+            <img alt="Jerusalem in Eid prayer" src="../../assets/img/homepage/Jerusalem-in-Eid-prayer-final.png"
+              class="rounded-2xl shadow-2xl" />
+          </div>
+          <div
+            class="col-span-3 2xl:mt-28 xl:mt-28 lg:mt-28 2xl:ml-20 xl:ml-20 lg:ml-20 2xl:mr-20 xl:mr-20 lg:mr-20 md:m-10 md:my-5 xs:my-5">
+            <p class="text-lg sm:text-lg xs:text-base text-white">
+              The Palestinian food market is a vibrant tapestry of flavors,
+              aromas, and traditions that reflect the region's rich culinary
+              heritage. From bustling souks to family-owned stalls, the market
+              offers an array of fresh produce, spices, and delicacies that
+              tantalize the senses and showcase the diversity of Palestinian
+              cuisine.
+            </p>
+            <p class="text-lg sm:text-lg xs:text-base text-white 2xl:mt-10 lg:mt-10 sm:mt-5 xs:mt-5">
+              Visitors to the Palestinian food market are treated to a feast for
+              the senses, with vendors skillfully preparing traditional dishes
+              like falafel, hummus, and maqluba, alongside an assortment of
+              sweets like knafeh and baklava. The market serves as a cultural
+              hub where locals and tourists alike come together to savor the
+              essence of Palestinian gastronomy, fostering a sense of community
+              and connection through shared meals and culinary experiences.
+            </p>
+          </div>
+          <img alt="candy" src="../../assets/img/homepage/candy-final.png"
+            class="rounded-2xl shadow-2xl 2xl:mt-0 xl:-mt-4 lg:mt-12 md:mt-0 2xl:ml-84 xl:ml-84 lg:ml-68 md:ml-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2" />
+          <img alt="Jerusalem in Eid" src="../../assets/img/homepage/Jerusalem-in-Eid-final.png"
+            class="rounded-2xl shadow-2xl 2xl:-mt-40 xl:-mt-20 lg:mt-1 2xl:ml-96 xl:ml-96 lg:ml-80 md:ml-0 sm:ml-auto xs:ml-auto 2xl:col-span-2 xl:col-span-2 lg:col-span-2" />
         </div>
-        <div
-          class="col-span-3 2xl:mt-28 xl:mt-28 lg:mt-28  2xl:ml-20 xl:ml-20 lg:ml-20 2xl:mr-20 xl:mr-20 lg:mr-20 md:m-10 md:my-5 xs:my-5"
-        >
-          <p class="text-lg sm:text-lg xs:text-base text-white">
-            The Palestinian food market is a vibrant tapestry of flavors,
-            aromas, and traditions that reflect the region's rich culinary
-            heritage. From bustling souks to family-owned stalls, the market
-            offers an array of fresh produce, spices, and delicacies that
-            tantalize the senses and showcase the diversity of Palestinian
-            cuisine.
-          </p>
-          <p
-            class="text-lg sm:text-lg xs:text-base text-white 2xl:mt-10 lg:mt-10 sm:mt-5 xs:mt-5"
-          >
-            Visitors to the Palestinian food market are treated to a feast for
-            the senses, with vendors skillfully preparing traditional dishes
-            like falafel, hummus, and maqluba, alongside an assortment of sweets
-            like knafeh and baklava. The market serves as a cultural hub where
-            locals and tourists alike come together to savor the essence of
-            Palestinian gastronomy, fostering a sense of community and
-            connection through shared meals and culinary experiences.
-          </p>
-        </div>
-        <img
-          alt="candy"
-          src="../../assets/img/homepage/candy-final.png"
-          class="rounded-2xl shadow-2xl 2xl:mt-0 xl:-mt-4 lg:mt-12 md:mt-0 2xl:ml-84 xl:ml-84 lg:ml-68 md:ml-1 2xl:col-span-2 xl:col-span-2 lg:col-span-2"
-        />
-        <img
-          alt="Jerusalem in Eid"
-          src="../../assets/img/homepage/Jerusalem-in-Eid-final.png"
-          class="rounded-2xl shadow-2xl 2xl:-mt-40 xl:-mt-20 lg:mt-1 2xl:ml-96 xl:ml-96 lg:ml-80 md:ml-0 sm:ml-auto xs:ml-auto 2xl:col-span-2 xl:col-span-2 lg:col-span-2"
-        />
-      </div>
       </div>
     </section>
   </div>
@@ -244,17 +196,24 @@ import TheBanner from "../../components/layout/TheBanner.vue";
 import { ref, onMounted } from "vue";
 import Card from "primevue/card";
 
+import "primeicons/primeicons.css";
+import Button from "primevue/button";
+
 import maamoul2Full from "../../assets/img/homepage/maamoul2-full.png";
 import maamoul4 from "../../assets/img/homepage/maamoul4.jpeg";
 import maamoul3 from "../../assets/img/homepage/maamoul3.jpeg";
 import maamoul from "../../assets/img/homepage/maamoul-full-final.png";
+import { useRoute, useRouter } from 'vue-router'
 
+const router = useRouter()
+const route = useRoute()
 
 export default defineComponent({
   name: "Basic",
   components: {
     TheBanner,
     Card,
+    Button,
   },
   data() {
     return {
@@ -295,6 +254,20 @@ export default defineComponent({
         this.imageSrc = maamoul4;
       }
     },
+    ReadAboutHistory() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+      this.$router.push('/history')
+    },
+    ReadAboutFood() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+      this.$router.push('/food')
+    }
   },
 });
 </script>
@@ -311,17 +284,11 @@ export default defineComponent({
   --dot-color: #ad8c83;
   --dot-size: 3.1px;
   --dot-space: 9px;
-  background: linear-gradient(
-        90deg,
-        var(--dot-bg) calc(var(--dot-space) - var(--dot-size)),
-        transparent 1%
-      )
-      center / var(--dot-space) var(--dot-space),
-    linear-gradient(
-        var(--dot-bg) calc(var(--dot-space) - var(--dot-size)),
-        transparent 1%
-      )
-      center / var(--dot-space) var(--dot-space),
+  background: linear-gradient(90deg,
+      var(--dot-bg) calc(var(--dot-space) - var(--dot-size)),
+      transparent 1%) center / var(--dot-space) var(--dot-space),
+    linear-gradient(var(--dot-bg) calc(var(--dot-space) - var(--dot-size)),
+      transparent 1%) center / var(--dot-space) var(--dot-space),
     var(--dot-color);
 }
 
@@ -348,4 +315,7 @@ export default defineComponent({
 .w-100 {
   width: 30rem;
 }
+
+
+
 </style>
